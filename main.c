0c7e7d82beea70746f07244bde7a2aaf3333859a
Damian Lambrecht <damianlamrecht@gmail.com>
2022-11-02 17:54:06 -0300

    funcion de suma

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 int main()
 {
     hola();
+    printf("suma: %d",suma(4,6));
+
+
     return 0;
 }
 
 void hola(){
     printf("Hello world!\n");
     printf("Hello world!\n");
+}
+
+int suma(int a,int b){
+    return a+b;
 }
